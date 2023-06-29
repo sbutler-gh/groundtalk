846ddabf0a4e953d8c3ae87211352fa7b2689f77
sbutler-gh <sam@ysli.be>
2023-06-29 20:44:05 +0200

    Updated html swap and links

--- functions/signUp.js
+++ functions/signUp.js
@@ -65,6 +65,16 @@ exports.handler = async (event) => {
   } else {
     console.log(JSON.stringify(data))
 
+    let copy_text = `We need low and resilient infrastructures.  We need our own social media and networks, if we want them.
+
+    And we need to organize to make it happen.
+    
+    Want a network for ourselves — built for low-energy and resilience, led by workers and people, in service of the worlds we want to make?
+    
+    Join to get involved, at one of the smallest websites on the Internet:
+    
+    https://groundtalk.land`;
+
     let swap = `
     <div class="swap" style="max-width: 600px; margin: auto;">
   <h2>Welcome!</h2>
@@ -77,32 +87,13 @@ exports.handler = async (event) => {
 <p><strong>And that's why this will be different.</strong>
 
 <p style="line-height:29px">You can invite people via <a href="mailto:?subject=A%20low-tech%20social%20media%20cooperative%2C%20want%20to%20join%3F&body=This%20is%20a%20cool%20project%20I%20found%20%E2%80%93%20https%3A%2F%2Fgroundtalk.land.%20%20Here%20are%20more%20details%2C%20anybody%20can%20join%2C%20and%20I%20wanted%20to%20invite%20some%20people%20who%20I%20want%20to%20be%20part%20of%20this%20...%0A%0AWe%20need%20low%20and%20resilient%20infrastructures.%20%20We%20need%20our%20own%20social%20media%20and%20networks%2C%20if%20we%20want%20them.%0A%0AAnd%20we%20need%20to%20organize%20to%20make%20it%20happen.%0A%0AWant%20a%20network%20for%20ourselves%20%E2%80%94%20built%20for%20low-energy%20and%20resilience%2C%20led%20by%20workers%20and%20people%2C%20in%20service%20of%20the%20worlds%20we%20want%20to%20make%3F%0A%0AJoin%20to%20get%20involved%2C%20at%20one%20of%20the%20smallest%20websites%20on%20the%20Internet%3A%0A%0Ahttps%3A%2F%2Fgroundtalk.land">✉️ email</a>, 
-  <a href="">🐦 Twitter</a>, <a href="">🐘 Mastodon</a>, <a href="">Facebook</a>, or <button onclick="copyContent()">copy an invite message</button> to post anywhere.</p>
+  <a target="_blank" href="https://twitter.com/sambutlerUS/status/1674486797756145687">🐦 Twitter</a>, <a href="">🐘 Mastodon</a>, <a href="">Facebook</a>, or <button onclick="copyContent()">copy an invite message</button> to post anywhere.</p>
 
 <p>💬 Want to introduce and connect with others involved?  <a href="https://www.linen.dev/s/people/c/welcome">Join the chat</a> — <em>1000x lighter than Slack!</em>
 
 <p>📚 Want to help build a library of resources — stories, games, events, education, tools, art, collections, proposals, and more?  <a href="https://docs.google.com/spreadsheets/d/1oaA7dLz8Scv3mH42Jq_jjfDLvKk4D3umrCArQomqMCE/edit?usp=sharing">Go to the library.</a>
 <br>
 <br>
-    <script>
-      let text = 'We need low and resilient infrastructures.  We need our own social media and networks, if we want them.
-
-And we need to organize to make it happen.
-
-Want a network for ourselves — built for low-energy and resilience, led by workers and people, in service of the worlds we want to make?
-
-Join to get involved, at one of the smallest websites on the Internet:
-
-https://groundtalk.land'
-      const copyContent = async () => {
-        try {
-          await navigator.clipboard.writeText(text);
-          console.log('Content copied to clipboard');
-        } catch (err) {
-          console.error('Failed to copy: ', err);
-        }
-      }
-    </script>
 `
     return {
       statusCode: 200,
