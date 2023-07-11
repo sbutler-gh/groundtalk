012d84068aea7e0bdd3928b9c83cbe643cfa153f
sbutler-gh <sam@ysli.be>
2023-07-12 00:45:51 +0200

    caches messages locally, loads on page load, and only retrieves newer messages

--- functions/getMessages.js
+++ functions/getMessages.js
@@ -13,32 +13,71 @@ exports.handler = async (event) => {
       return { statusCode: 405, body: 'Method Not Allowed' };
     }
 
-    let { data: messages, error } = await supabase
-    .from('messages')
-    .select('*')
+    console.log(event.queryStringParameters.ts);
+    // console.log(event.httpMethod);
+    let ts = event.queryStringParameters.ts.substring(0,26);
+    console.log(ts);
 
-  
-  if (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error),
-    };
-  } else {
-    console.log(messages);
+    if (ts != 0) {
 
-    let swap = "";
+        let { data: messages, error } = await supabase
+        .from('messages')
+        .select('*')
+        .gt('ts', ts);
 
-    for (var i= messages.length - 1; i >= 0; i--) {
-        swap = swap + `<div class="post">
-            <p class="by">${messages[i].by}
-            <p class="ts">${messages[i].ts}
-            <p>${messages[i].txt}
-        </div><br>`
+        if (error) {
+            return {
+              statusCode: 500,
+              body: JSON.stringify(error),
+            };
+          } else {
+            console.log(messages);
+        
+            let swap = "";
+        
+            for (var i= messages.length - 1; i >= 0; i--) {
+                swap = swap + `<div class="post">
+                    <p class="by">${messages[i].by}
+                    <p class="ts">${messages[i].ts}
+                    <p>${messages[i].txt}
+                </div><br>`
+            }
+        
+            return {
+              statusCode: 200,
+              body: (JSON.stringify(messages), swap)
+            };
+          }
+
+    }
+    else {
+        let { data: messages, error } = await supabase
+        .from('messages')
+        .select('*')
+
+        if (error) {
+            return {
+              statusCode: 500,
+              body: JSON.stringify(error),
+            };
+          } else {
+            console.log(messages);
+        
+            let swap = "";
+        
+            for (var i= messages.length - 1; i >= 0; i--) {
+                swap = swap + `<div class="post">
+                    <p class="by">${messages[i].by}
+                    <p class="ts">${messages[i].ts}
+                    <p>${messages[i].txt}
+                </div><br>`
+            }
+        
+            return {
+              statusCode: 200,
+              body: (JSON.stringify(messages), swap)
+            };
+          }
     }
 
-    return {
-      statusCode: 200,
-      body: (JSON.stringify(messages), swap)
-    };
-  }
   };
